43ef2726d220f1cc5daf811ed6fe8fc9afa9fd4f
Karim Arhan <karim.arhan@yahoo.com>
2021-09-22 14:59:52 +0700

    - Finishing viewController storyboard

---
+++ Trending Git/RepoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RepoCollectionViewCell.swift
+//  Trending Git
+//
+//  Created by Karim Arhan on 22/09/21.
+//  Copyright © 2021 Karim Arhan. All rights reserved.
+//
+
+import UIKit
+
+class RepoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var repoImg: UIImageView!
+    @IBOutlet weak var repoNameLbl: UILabel!
+    @IBOutlet weak var starLbl: UILabel!
+    @IBOutlet weak var forkLbl: UILabel!
+    @IBOutlet weak var FSView: UIView!
+    
+}
--- Trending Git/ViewController.swift
+++ Trending Git/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
